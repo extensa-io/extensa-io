06a841a048213ce20d5f55f940e87140a709c193
extensa-io <nestor@extensa.io>
2025-07-22 21:52:11 -0400

    Initial Next.js setup with TypeScript and Tailwind CSS

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1>Néstor Daza</h1>
+        <p>Kitchen - Fútbol - Software</p>
+      </div>
+    </main>
+  )
+}
